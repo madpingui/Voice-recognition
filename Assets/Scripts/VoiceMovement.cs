4cca94f00878e4f3e207237593b0c4ace49bf43e
Juan Manuel <68016784+madpingui@users.noreply.github.com>
2022-04-23 00:13:53 -0500

    Initial commit

---
+++ Assets/Scripts/VoiceMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class VoiceMovement : MonoBehaviour
+{
+    public Text textDebug;
+
+    public string[] keywords;
+    public ConfidenceLevel confidence = ConfidenceLevel.Medium;
+
+    protected PhraseRecognizer recognizer;
+    protected string word;
+
+    private void Start()
+    {
+        if (keywords != null)
+        {
+            recognizer = new KeywordRecognizer(keywords, confidence);
+            recognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
+            recognizer.Start();
+        }
+    }
+
+    private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        word = args.text;
+        textDebug.text = "You said: <b>" + word + "</b>";
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (recognizer != null && recognizer.IsRunning)
+        {
+            recognizer.OnPhraseRecognized -= Recognizer_OnPhraseRecognized;
+            recognizer.Stop();
+        }
+    }
+}
